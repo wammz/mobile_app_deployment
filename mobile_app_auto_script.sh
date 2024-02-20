e87f6d251f342eff367103b55fef801e43447f5d
Fannuel Wamambo <fwamambo@gmail.com>
2024-02-20 08:56:52 +0200

    new edits, informative echo messages

--- mobile_app_auto_script.sh
+++ mobile_app_auto_script.sh
@@ -9,7 +9,10 @@ tarball=mobile_app_backup.tar.xz
 source_dir=/home/administrator/mobile_app_backup
 dest_dir=/var/lib/mrs/
 mobile_home_dir=/var/lib/mrs/mobile_app_backup
+# change this before running
 site_code=ZW006654
+echo -e "***************************************\n 
+This ensure you have changed the site code to match this facility\n*********************"
 tar -xfv $tarball
 rm -f $tarball
 # move these resources into /home/administrator
@@ -24,15 +27,15 @@ docker load -i $source_dir/backup-infrastructure.tar
 # cd into /var/lib/mrs
 cd $dest_dir
 #bring down EHR services
-echo "Bringing EHR down...."
+echo "Bringing EHR down....\n"
 docker-compose down
 echo "EHR down...Please check your compose file and change accordingly \n"
 # bring docker up
 docker-compose up -d
 # cd into mobile app dir
 cd $mobile_home_dir
-echo "edit env file....change site_id under mobile client to match this site \n"
-echo "change credentials for the mobile app user\n"
+echo -e "Please edit env file....\nchange site_id under mobile client to match this site \n"
+echo -e "change credentials for the mobile app user\n"
 # bring up docker
 docker-compose up -d
 #
@@ -40,10 +43,12 @@ docker-compose up -d
 cd $mobile_home_dir/connectors/
 # edit site id in the activate_debezium_mysql_connector.sh
 # modify site_id accordingly
+echo -e "Please ensure site code in this file (activate_debezium_mysql_connector) matches the facility site code\n"
 #edit activate_http_sink_connector.sh
 #edit site_id accordingly
+echo -e "Assuming site code in this file (activate_http_sink_connector is fine\n"
 # get into mysql
-# this to create a db in mysql
+# edit this to create a db in mysql
 # create mysql script with table creation
 docker exec -it $site_code-mysql mysql 
 create database mobile;
